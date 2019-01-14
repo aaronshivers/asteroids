bcff088acbd96d33a68bc2f0fdb1f8386b9f8e6a
Aaron Shivers <aaron@moonmail.org>
2019-01-14 17:21:19 -0600

    Added Start Script

--- asteroids.js
+++ asteroids.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('index.html'))
 
 app.listen(port)
